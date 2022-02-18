364721779101469566a4d4219aef82d05271c0d9
Celinaki <celina1999@live.se>
2022-02-18 09:56:53 +0100

    Testing conflict

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <h1>Celina</h1>
+      <h1>Celina oki</h1>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
