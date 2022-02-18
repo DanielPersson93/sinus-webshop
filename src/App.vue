b9e7165e89b65be253dd6ad80c3f5b1365eb7d12
PeterKron <peter.kronqvist@hotmail.se>
2022-02-18 09:50:24 +0100

    testing conflict two

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <p>här är min gröna p tagg</p>
     </div>
     <router-view/>
   </div>
