129dd8fe58042419ac379c8e5298adac58b5234f
Daniel Persson <daniel.persson93@hotmail.se>
2022-02-22 13:13:14 +0100

    sign in form

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.baseURL = "http://localhost:5000/api";
 
-export async function getAllProducts(){
-    return await axios.get('/items/')
+export async function getAllProducts() {
+  return await axios.get("/items/");
 }
-export async function fetchCategory(grillkorv){
-    return await axios.get('/items/?category='+`${grillkorv}`)
+export async function fetchCategory(grillkorv) {
+  return await axios.get("/items/?category=" + `${grillkorv}`);
 }
-export async function fetchApparel(){
-    return await axios.get('/items/?exclude=skateboard,wheel')
+export async function fetchApparel() {
+  return await axios.get("/items/?exclude=skateboard,wheel");
+}
+export async function saveUser(user) {
+  console.log("API", user);
+  //   return await axios.post("/register/", { user });
+  return await axios.post(
+    "/register/",
+    { body: user },
+    { headers: { "Content-Type": "application/json" } }
+  );
 }
 
 // http://localhost:5000/api/items?exclude=wheel,skateboard&page=1
--- src/store/index.js
+++ src/store/index.js
@@ -1,49 +1,63 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import * as API from '@/api'
+import Vue from "vue";
+import Vuex from "vuex";
+import * as API from "@/api";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    query:"",
-    products:["kalle","kalas","karl","3","4","5"],
-    allProducts: []
+    query: "",
+    products: ["kalle", "kalas", "karl", "3", "4", "5"],
+    allProducts: [],
+    user: {},
   },
   mutations: {
-    setQuery(state,input){
-      state.query=input
+    setQuery(state, input) {
+      state.query = input;
     },
-    saveProducts(state, allProducts){
-      state.allProducts = allProducts
+    saveProducts(state, allProducts) {
+      state.allProducts = allProducts;
       // console.log("testernas test")
       // console.log(allProducts)
     },
+    saveUser(state, user) {
+      state.user = user;
+      console.log("hej", state.user);
+    },
   },
-  getters:{
-    resultsLimited(state, ){
-      return state.products.filter(product => product.toUpperCase() == state.query.toUpperCase())
+  getters: {
+    resultsLimited(state) {
+      return state.products.filter(
+        (product) => product.toUpperCase() == state.query.toUpperCase()
+      );
       // arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()
-      // return state.products.filter(product => product[0] == state.query[0])  
-    }
+      // return state.products.filter(product => product[0] == state.query[0])
+    },
   },
 
   actions: {
-/*     async fetchAllProducts(context){
+    /*     async fetchAllProducts(context){
       const response = await API.getAllProducts()
       context.commit('saveProducts', response.data.products)
       console.log(response.data.products)
     }, */
-    async getProductsAction(context, payloadCategory){
-        const response = await API.fetchCategory(payloadCategory)
-        context.commit('saveProducts', response.data.products) 
+    async getProductsAction(context, payloadCategory) {
+      const response = await API.fetchCategory(payloadCategory);
+      context.commit("saveProducts", response.data.products);
     },
-    async fetchApparel(context){
-      const response = await API.fetchApparel()
-      context.commit('saveProducts', response.data.products) 
-      console.log(response.data.products)
-  }
+    async fetchApparel(context) {
+      const response = await API.fetchApparel();
+      context.commit("saveProducts", response.data.products);
+      console.log(response.data.products);
+    },
+    registerUser(context, user) {
+      context.commit("saveUser", user);
+      API.saveUser(user);
+      // console.log("response", response);
+
+      // const response = API.saveUser();
+      // console.log(response.data);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
