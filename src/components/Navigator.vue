c9e5d41992c24a2017718c2a5562d53753469399
danielpersson93 <daniel.persson93@hotmail.se>
2022-02-24 09:30:53 +0100

    signup form template, style and script added

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,207 +1,235 @@
 <template>
-  <div class="nav-wrap"> 
-    <div class="abso"> 
-      <Login> </Login> 
-    </div>   
-    <img src="@/assets/sinuslogo.svg" alt="" class="sinus-logo" />
-    <section class="links">
-    <router-link to="/">Home</router-link>
+	<div class="nav-wrap">
+		<div class="abso">
+			<Login> </Login>
+		</div>
+		<img src="@/assets/sinuslogo.svg" alt="" class="sinus-logo" />
+		<section class="links">
+			<router-link to="/">Home</router-link>
 
-      <div class="category" @mouseover="hoverSkate = true" @mouseleave="hoverSkate = false">
-        <router-link to="/productview" class="par"> Skates</router-link>
+			<div
+				class="category"
+				@mouseover="hoverSkate = true"
+				@mouseleave="hoverSkate = false"
+			>
+				<router-link to="/productview" class="par"> Skates</router-link>
 
-        <ul class="theUl" v-if="hoverSkate">
-          <router-link to="/productview">
-            <li class="listI" @click="assignCategory('skateboard')">Boards</li>
-          </router-link>
-          <router-link to="/productview">
-            <li class="listI" @click="assignCategory('wheel')">Wheels</li>
-          </router-link>
-        </ul>
-      </div>
+				<ul class="theUl" v-if="hoverSkate">
+					<router-link to="/productview">
+						<li class="listI" @click="assignCategory('skateboard')">Boards</li>
+					</router-link>
+					<router-link to="/productview">
+						<li class="listI" @click="assignCategory('wheel')">Wheels</li>
+					</router-link>
+				</ul>
+			</div>
 
-      <div class="category" @mouseover="hoverApparel = true" @mouseleave="hoverApparel = false">
-        <router-link to="/productview" class="par"> Apparel</router-link>
+			<div
+				class="category"
+				@mouseover="hoverApparel = true"
+				@mouseleave="hoverApparel = false"
+			>
+				<router-link to="/productview" class="par"> Apparel</router-link>
 
-        <ul class="theUl" v-if="hoverApparel">
-          <router-link :to="{ name: 'ProductView', query: { category: clicked } }">
-            <li class="listI" @click="setClicked">Hoodies</li>
-          </router-link>
+				<ul class="theUl" v-if="hoverApparel">
+					<router-link
+						:to="{ name: 'ProductView', query: { category: clicked } }"
+					>
+						<li class="listI" @click="setClicked">Hoodies</li>
+					</router-link>
 
-          <router-link :to="{ name: 'ProductView', query: { category: clicked } }">
-            <li class="listI" @click="setClicked">T-shirts</li>
-          </router-link>
-          <router-link :to="{ name: 'ProductView', query: { category: clicked } }">
-            <li class="listI" @click="setClicked">Socks</li>
-          </router-link>
-          <router-link to="/productview">
-            <li class="listI">Bags</li>
-          </router-link>
-          <router-link to="/productview">
-            <li class="listI">Headwear</li>
-          </router-link>
-        </ul>
-      </div>
-    </section>
+					<router-link
+						:to="{ name: 'ProductView', query: { category: clicked } }"
+					>
+						<li class="listI" @click="setClicked">T-shirts</li>
+					</router-link>
+					<router-link
+						:to="{ name: 'ProductView', query: { category: clicked } }"
+					>
+						<li class="listI" @click="setClicked">Socks</li>
+					</router-link>
+					<router-link to="/productview">
+						<li class="listI">Bags</li>
+					</router-link>
+					<router-link to="/productview">
+						<li class="listI">Headwear</li>
+					</router-link>
+				</ul>
+			</div>
+		</section>
 
-    <section class="search-field">
-      <span class="material-icons-outlined cursor" @click="search"> search </span>
+		<section class="search-field">
+			<span class="material-icons-outlined cursor" @click="search">
+				search
+			</span>
 
-      <section class="result">
-        <input type="text" placeholder="Search.." @keyup="setQuery" @keyup.enter="search" v-model="searchInput" class="search-results-parent"/>
-        <ul class="search-results">
-          <li v-for="(product, index) in $store.getters.resultsLimited"
-            :key="index"
-            class="result-items"
-            @click="temp">
-            {{ product }}
-          </li>
-        </ul>
-      </section>
+			<section class="result">
+				<input
+					type="text"
+					placeholder="Search.."
+					@keyup="setQuery"
+					@keyup.enter="search"
+					v-model="searchInput"
+					class="search-results-parent"
+				/>
+				<ul class="search-results">
+					<li
+						v-for="(product, index) in $store.getters.resultsLimited"
+						:key="index"
+						class="result-items"
+						@click="temp"
+					>
+						{{ product }}
+					</li>
+				</ul>
+			</section>
 
-      <span class="material-icons-outlined"> shopping_bag </span>
-      <span class="material-icons-outlined" @click="openLogin"> person_outline </span>
-      <!-- <p>{{getQuery}}</p> -->
-    </section>
-  </div>
+			<span class="material-icons-outlined" @click="toggleLoginModal">
+				shopping_bag
+			</span>
+			<span class="material-icons-outlined" @click="openLogin">
+				person_outline
+			</span>
+			<!-- <p>{{getQuery}}</p> -->
+		</section>
+		<SignUp v-if="isLoginModalOpen" />
+	</div>
 </template>
 
 <script>
-import Login from '@/components/Login.vue'
+	import SignUp from "./SignUp.vue";
+	import Login from "@/components/Login.vue";
 
-export default {
-  components:{Login },
-  data() {
-    return {
-      searchInput: "",
-      hoverApparel: false,
-      hoverSkate: false,
-      clicked: "",
-    };
-  },
-  methods: {
-    setQuery() {
-      this.$store.commit("setQuery", this.searchInput);
-    },
-    setClicked(e) {
-      this.clicked = e.target.innerText.toLowerCase();
-    },
-    openLogin(){
-      console.log("HEJ")
-    },
-    search() {
-      this.$router.push({
-        name: "Products",
-        query: { "": this.searchInput },
-      });
-    },
-    assignCategory(category){
-      this.$store.dispatch("getProductCategory", category)
-      console.log(category)
-    },
-  },
-  computed: {
-    getQuery() {
-      return this.$store.state.query;
-    },
-  },
-};
+	export default {
+		components: { Login, SignUp },
+		data() {
+			return {
+				searchInput: "",
+				hoverApparel: false,
+				hoverSkate: false,
+				clicked: "",
+				isLoginModalOpen: false,
+			};
+		},
+		methods: {
+			toggleLoginModal() {
+				this.isLoginModalOpen = !this.isLoginModalOpen;
+			},
+			setQuery() {
+				this.$store.commit("setQuery", this.searchInput);
+			},
+			setClicked(e) {
+				this.clicked = e.target.innerText.toLowerCase();
+			},
+			openLogin() {
+				console.log("HEJ");
+			},
+			search() {
+				this.$router.push({
+					name: "Products",
+					query: { "": this.searchInput },
+				});
+			},
+			assignCategory(category) {
+				this.$store.dispatch("getProductCategory", category);
+				console.log(category);
+			},
+		},
+		computed: {
+			getQuery() {
+				return this.$store.state.query;
+			},
+		},
+	};
 </script>
 
 <style scoped>
-.material-icons-outlined {
-  font-size: 1.8rem;
-  color: black; 
-   cursor: pointer;
+	.material-icons-outlined {
+		font-size: 1.8rem;
+		color: black;
+		cursor: pointer;
+	}
+	/* .cursor{
+									} */
+	.nav-wrap {
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		font-size: 24px;
+		display: flex;
+		padding: 1rem 2.5rem;
+		align-items: center;
+		justify-content: space-between;
+		font-weight: 600;
+		position: relative;
+	}
 
-}
-/* .cursor{
-} */
-.nav-wrap {
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  font-size: 24px;
-  display: flex;
-  padding: 1rem 2.5rem;
-  align-items: center;
-  justify-content: space-between;
-  font-weight: 600;
-    position: relative;
-}
-/* .apparel-ul , .skate-ul{
-  display: none;
-}
-.apparel-link:hover{
-  display:contents;
-} */
-.links {
-  display: flex;
-  justify-content: space-between;
-  gap: 7rem;
-}
+	.links {
+		display: flex;
+		justify-content: space-between;
+		gap: 7rem;
+	}
 
-input {
-  padding: 0.4rem 1rem;
-  border-radius: 10px;
-  border: solid black 2.5px;
-}
+	input {
+		padding: 0.4rem 1rem;
+		border-radius: 10px;
+		border: solid black 2.5px;
+	}
 
-.search-field {
-  margin-left: 20rem;
-  width: 23rem;
-  display: flex;
-  justify-content: space-between;
-    position: relative;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
+	.search-field {
+		margin-left: 20rem;
+		width: 23rem;
+		display: flex;
+		justify-content: space-between;
+		position: relative;
+	}
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
 
+	.search-results-parent,
+	.category {
+		position: relative;
+		z-index: 1000;
+	}
 
-.search-results-parent,
-.category {
-  position: relative;
-  z-index: 1000;
-}
+	.search-results {
+		position: absolute;
+		top: 10%;
+		/* left: 79%; */
+		z-index: 90000;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		background-color: rgb(255, 255, 255);
+		width: 13%;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	}
+	.par {
+		z-index: 9000;
+	}
 
-.search-results {
-  position: absolute;
-  top: 10%;
-  /* left: 79%; */
-  z-index: 90000;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  background-color: rgb(255, 255, 255);
-  width: 13%;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-.par {
-  z-index: 9000;
-}
-
-.theUl {
-  position: absolute;
-  padding-top: 5rem;
-  top: 80%;
-  left: 0%;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  background-color: rgb(255, 255, 255);
-  width: 100%;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-.result-items:hover,
-.listI:hover {
-  background-color: rgba(68, 67, 67, 0.2);
-  width: 100%;
-}
-.result-items,
-.listI {
-  transition: 0.8s;
-  text-align: left;
-  padding-top: 0.8rem;
-  width: inherit;
-}
+	.theUl {
+		position: absolute;
+		padding-top: 5rem;
+		top: 80%;
+		left: 0%;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		background-color: rgb(255, 255, 255);
+		width: 100%;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	}
+	.result-items:hover,
+	.listI:hover {
+		background-color: rgba(68, 67, 67, 0.2);
+		width: 100%;
+	}
+	.result-items,
+	.listI {
+		transition: 0.8s;
+		text-align: left;
+		padding-top: 0.8rem;
+		width: inherit;
+	}
 </style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,31 +1,197 @@
 <template>
-  <div class="signup">
-        <h1>Sign up</h1>
-        <img src="@/assets/cross-icon.png" alt="">
-        <label for=""></label>
-        <input type="text" name="name" id="">
-        <label for=""></label>
-        <input type="text" name="street" id="">
-        <label for=""></label>
-        <input type="text" name="zip" id="">
-        <label for=""></label>
-        <input type="text" name="city" id="">
-        <label for=""></label>
-        <input type="text" name="email" id="">
-        <label for=""></label>
-        <input type="password" name="password" id="">
-        <label for=""></label>
-        <input type="password" name="repeatpassword" id="">
-        <button>SIGN UP</button>
-  </div>
+	<section id="login-form">
+		<h3>Register form</h3>
+
+		<form action="#">
+			<div class="d-flex">
+				<div class="section-2">
+					<div class="form-control">
+						<label for="name">Name</label>
+						<input type="text" id="name" v-model="registerFormData.name" />
+					</div>
+					<div class="form-control">
+						<label for="street">Street</label>
+						<input
+							type="text"
+							id="street"
+							v-model="registerFormData.address.street"
+						/>
+					</div>
+					<div class="zipCity">
+						<div class="form-control">
+							<label for="city">City</label>
+							<input
+								type="text"
+								id="city"
+								v-model="registerFormData.address.city"
+							/>
+						</div>
+						<div class="form-control">
+							<label for="zipcode">Zip Code</label>
+							<input
+								minlength="5"
+								maxlength="5"
+								type="text"
+								id="zipcode"
+								v-model="registerFormData.address.zip"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div class="section-2">
+					<div class="form-control">
+						<label for="email">Email</label>
+						<input type="email" id="email" v-model="registerFormData.email" />
+					</div>
+					<div class="form-control">
+						<label for="password">Password</label>
+						<input
+							minlength="6"
+							type="password"
+							id="password"
+							v-model="registerFormData.password"
+						/>
+					</div>
+					<div class="form-control">
+						<label for="repeat-password">Repeat password</label>
+						<input
+							minlength="6"
+							type="password"
+							id="repeat-password"
+							v-model="repeatedPassword"
+						/>
+					</div>
+				</div>
+			</div>
+			<button @click="signUp()">Sign up</button>
+		</form>
+	</section>
 </template>
 
 <script>
-export default {
+	import { registerUser } from "../api/index.js";
+	export default {
+		data() {
+			return {
+				repeatedPassword: "",
+				/** Form that backend excepts */
+				registerFormData: {
+					email: "",
+					password: "",
+					name: "",
+					address: {
+						city: "",
+						street: "",
+						zip: "",
+					},
+				},
+			};
+		},
+		methods: {
+			signUp() {
+				// Error handeling needed
+				if (this.repeatedPassword !== this.registerFormData.password) {
+					// Show an error here?
+					return alert("Passwords does not match");
+				}
 
-}
+				/** Check if we are missing data */
+				if (
+					!this.registerFormData.email ||
+					!this.registerFormData.password ||
+					!this.registerFormData.name ||
+					!this.registerFormData.address.city ||
+					!this.registerFormData.address.street ||
+					!this.registerFormData.address.zip
+				) {
+					// Show an error here?
+					return alert("Invalid input data");
+				}
+
+				registerUser(this.registerFormData)
+					.then((registerRes) => {
+						// Store token in local storage
+						const token = registerRes.data.token;
+						localStorage.setItem("authToken", token);
+
+						// Store user details in local storage (?)
+						const createdUser = registerRes.data.user;
+						localStorage.setItem("user", JSON.stringify(createdUser));
+
+						// skicka till profil sidan när du är klar
+						//
+						// this.$route.go('/profile');
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			},
+		},
+	};
 </script>
 
-<style>
+<style lang="scss" scoped>
+	#login-form {
+		width: 794px;
+		height: 611px;
+		z-index: 30;
+		padding: 40px;
+		background: white;
+		border: 4px;
+		right: 10px;
+		position: absolute;
+		top: 80px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.14);
+		border-radius: 4px;
+		margin: 0 auto;
 
+		.d-flex {
+			display: flex;
+			justify-content: space-between;
+			label {
+				font-family: Mukta Malar;
+				font-style: normal;
+				font-weight: normal;
+				font-size: 16px;
+				line-height: 27px;
+				letter-spacing: -0.02em;
+				margin-top: 40px;
+			}
+			.zipCity {
+				display: flex;
+				justify-content: space-between;
+
+				div:nth-of-type(1) {
+					width: 25%;
+				}
+				div:nth-of-type(2) {
+					width: 70%;
+				}
+			}
+			.section-2 {
+				width: 40%;
+			}
+		}
+
+		.form-control {
+			margin-bottom: 14px;
+
+			input,
+			label {
+				display: block;
+			}
+			input {
+				box-sizing: border-box;
+				width: 100%;
+				padding: 1rem;
+				border: 1px solid #ccc;
+				border-radius: 4px;
+			}
+			input:focus {
+				outline-color: transparent;
+				outline-style: none;
+			}
+		}
+	}
 </style>
