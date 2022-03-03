227f455f7241308a9a069477e3f524a217446ae1
PeterKron <peter.kronqvist@hotmail.se>
2022-03-03 09:17:38 +0100

    hej

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,79 +1,60 @@
 <template>
-	<div class="home">
-		<article class="grid-wrap">
-				<article class="grid-full-width" @click="getSingleProduct(16)">
-					<img src="@/assets/hero.jpg" alt="" />
-					<section class="hero-left">
-						<h1>Plastic 1</h1>
-						<h2>now in plastic</h2>
-					</section>
-					<section class="hero-right">
-						<h3>SHOP NOW</h3>
-					</section>
-				</article>
+<div class="home">
+      <div class="hero"  @click="getSingleProduct(16)">
+        <aside>
+          <h1>Plastic-1</h1>
+          <h2>now in plastic</h2>
+        </aside>
+        <img src="../assets/sinus-skateboard-plastic 1.png" alt="">
+        <h3>SHOP NOW</h3>
+      </div>
 
-			<article class="picture-parent">
-				<img src="@/assets/boardsbox.jpg" alt="" />
-				<section class="picture-child">
-					<h3>Boards</h3>
-					<router-link :to="{ name: 'ProductView', query: { category: 'skateboard' } }">
-						<button class="white-border-button" @click="assignCategory('skateboard')">
-							VIEW MORE
-						</button>
-					</router-link>
-				</section>
-			</article>
+      <div class="container">
+        <div class="boards">
+          <section>
+            <h3>Boards</h3>
+			<router-link :to="{ name: 'ProductView', query: { category: 'skateboard' } }">
+            <button class="whiteborderbutton">VIEW MORE</button>
+			</router-link>
+          </section>
+        </div>
 
-			<article class="grid-span-2 picture-parent">
-				<img src="@/assets/apparelbox.jpg" alt="" />
+        <div class="apparel">
+          <section>
+            <h3>Apparel</h3>
+			<router-link :to="{ name: 'ProductView', query: { category: 'socks' } }">
+            <button class="whiteborderbutton">VIEW MORE</button>
+			</router-link>
+          </section>
+        </div>
+      </div>
 
-				<section class="picture-child">
-					<h3>T-shirts</h3>
-					<router-link :to="{ name: 'ProductView', query: { category: 'tshirt' } }">
-						<button class="white-border-button" @click="assignCategory('tshirt')">
-							VIEW MORE
-						</button>
-					</router-link>
-				</section>
-			</article>
+      <div class="container">
+        <div class="wheels">
+          <section>
+            <h3>Wheels</h3>
+			<router-link :to="{ name: 'ProductView', query: { category: 'wheel' } }">
+            <button class="whiteborderbutton">VIEW MORE</button>
+			</router-link>
+          </section>
+        </div>
 
-			<article class="grid-span-2 picture-parent">
-				<img src="@/assets/wheelbox.jpg" alt="" />
-				<section class="picture-child">
-					<h3>Wheels</h3>
-					<router-link :to="{ name: 'ProductView', query: { category: 'wheel' } }">
-						<button class="white-border-button" @click="assignCategory('wheel')">
-							VIEW MORE
-						</button>
-					</router-link>
-				</section>
-			</article>
-
-			<article class="positioning bags__article">
-				<img class="greta-totebag" src="@/assets/totebag.png" alt="" />
-				<section class="bags__section">
-					<h3>Bags</h3>
-					<router-link :to="{ name: 'ProductView', query: { category: 'totebag' } }">
-						<button class="white-border-button" @click="assignCategory('totebag')">
-							VIEW MORE
-						</button>
-					</router-link>
-				</section>
-			</article>
-		</article>
-	</div>
+        <div class="bags">
+          <section>
+            <h3>Bags</h3>
+			<router-link :to="{ name: 'ProductView', query: { category: 'totebag' } }">
+            <button class="whiteborderbutton">VIEW MORE</button>
+			</router-link>
+          </section>
+          <img src="../assets/sinus-totebag-gretaedition 1.png" alt="">
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
-	// import Actions from '@/store/action.types.js'
 	export default {
 		methods: {
-			navigate(query) {
-				this.$router.push({
-					name: "ProductView",
-					query: { category: query },
-				});
-			},
 			assignCategory(category) {
 				this.$store.dispatch("getProductCategory", category);
 			},
@@ -90,84 +71,81 @@
 
 
 <style lang="scss" scoped>
-	h1,
-	h2,
-	h3 {
-		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-	}
+.container {
+  display: flex;
+}
+.home {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  color: white;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.apparel, .bags, .boards, .wheels, .hero {
+  display: flex;
+  margin-top: 80px;  
+  box-shadow:(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 16px;
+  // margin-right: 80px;
+    h3, button {
+    margin-left: 16px;
+    margin-top: 16px;
+  }
+}
+button:hover {
+	border: 2px solid #ffffff;
+	cursor: pointer;
+}
+.hero {
+  cursor: pointer;
+  align-items: center;
+  width: 1360px;
+  height: 363px;
+  background-image: url("../assets/jongsun-lee-F-pSZO_jeE8-unsplash.jpg");
+  background-size: 80%;
+  background-position-y: 55%;
+  background-position-x: 55%;
+  margin-top: 0px;
+  aside { margin: -100px 120px 0px 80px;}
+  img {margin-right: 200px;}
 
-	.home {
-		padding: 0 5rem;
-	}
-	img {
-		object-fit: cover;
-		width: 100%;
-		height: 100%;
-		position: relative;
-		box-shadow: (0px 4px 4px rgba(0, 0, 0, 0.25));
-		border-radius: 16px;
-		z-index: 0;
-	}
-
-	.grid-wrap {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-		gap: 5rem;
-		width: 90%;
-		margin: 5rem auto;
-	}
-	.grid-full-width {
-		grid-column: span 5;
-		color: white;
-		position: relative;
-	}
-
-	.picture-parent {
-		grid-column: span 2;
-		position: relative;
-		.picture-child {
-			position: absolute;
-			top: 2%;
-			left: 5%;
-			color: white;
-		}
-	}
-	.hero-left {
-		position: absolute;
-		top: 28%;
-		left: 15%;
-		text-align: center;
-	}
-	.hero-right {
-		position: absolute;
-		top: 40%;
-		right: 12%;
-	}
-
-	.grid-span-2 {
-		grid-column: span 3;
-	}
-	.bags__article {
-		grid-column: span 2;
-		display: flex;
-		flex-direction: row-reverse;
-		justify-content: space-between;
-		padding: 1rem 0 0 2rem;
-		background-image: url("../assets/bagsbox.jpg");
-		background-size: cover;
-		border-radius: 16px;
-		color: white;
-		box-shadow: (0px 4px 4px rgba(0, 0, 0, 0.25));
-		.bag__section {
-			margin: 0 0 0 0;
-		}
-	}
-	.greta-totebag {
-		margin: 0 40px 0 0;
-		height: 350px;
-		width: 200px;
-	}
-	article > *button {
-		margin-top: 2rem;
-	}
+}
+.boards, .bags {
+  width: 493px;
+  height: 418px;
+}
+.apparel, .wheels {
+  width: 787px;
+  height: 418px;
+}
+.apparel,.bags {margin-left: 80px;} 
+// .wheels,.bags {margin-bottom: 80px;} 
+.boards {
+  background-image: url("../assets/niket-nigde-9N3rS-xLfkI-unsplash.jpg");
+  background-size: 100%;
+  background-position-y: 75%;
+  // background-position-x: 50%;
+}
+.apparel {
+  // background-image: url("../assets/priscilla-du-preez-WacbrU86JIw-unsplash.jpg");
+  background-image: url("../assets/yasin-hosgor-7oHu7joUoUo-unsplash.jpg");
+  background-size: 100%;
+  // background-position-y: 10%;
+  // background-position-x: 90%;
+}
+.wheels {
+  // justify-content: flex-end;
+  // section {margin-right: 40px;}
+  background-image: url("../assets/liam-martens-Bh_hHUfPXJU-unsplash.jpg");
+  background-size: 100%;
+  background-position-y: 55%;
+  // background-position-x: -30%;
+}
+.bags {
+ img {margin-left: 15%;}
+  background-image: url("../assets/mika-baumeister-Bv_PMxkprVQ-unsplash.jpg");
+  background-size: 150%;
+  background-position-x: 80%;
+  background-position-y: 70%;
+}
 </style>
